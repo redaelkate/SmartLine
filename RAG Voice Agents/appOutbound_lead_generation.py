4c7938eed23a68cff5021c5fa1b737f76825c81d
Mouad En-nasiry <90518486+mouadenna@users.noreply.github.com>
2025-01-08 23:13:28 +0100

    Update appOutbound_lead_generation.py

--- RAG Voice Agents/appOutbound_lead_generation.py
+++ RAG Voice Agents/appOutbound_lead_generation.py
@@ -361,9 +361,9 @@ async def initialize_session(openai_ws):
 
 if __name__ == "__main__": # python appOutbound.py --call=+18005551212
     parser = argparse.ArgumentParser(description="Run the Twilio AI voice assistant server.")
-    parser.add_argument('--call', default="+212708279841", help="The phone number to call, e.g., '--call=+18005551212'")
+    parser.add_argument('--call', default=os.getenv("PHONE_NUMBER_TO"), help="The phone number to call, e.g., '--call=+18005551212'")
     args = parser.parse_args()
-    
+
     phone_number = args.call
     print(
         'Our recommendation is to always disclose the use of AI for outbound or inbound calls.\n'
