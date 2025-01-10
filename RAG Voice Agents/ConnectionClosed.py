64c8ae012d0c9a8cf0f566fcbac0a50b7dd6d77c
Mouad En-nasiry <90518486+mouadenna@users.noreply.github.com>
2025-01-10 17:46:49 +0100

    Update ConnectionClosed.py

--- RAG Voice Agents/ConnectionClosed.py
+++ RAG Voice Agents/ConnectionClosed.py
@@ -1,21 +1,41 @@
 import logging
-from services.openai_functions import generate_summary
+from tools.call_agents import generate_summary
+from tools.lead_generation_info import process_lead_conversation
 
-# Custom logging handler to watch for "connection closed"
 class ConnectionClosedHandler(logging.Handler):
-    def __init__(self, callback, transcript):
+    def __init__(self, callback, transcript,phone,name):
         super().__init__()
         self.callback = callback
         self.transcript = transcript
+        self.phone=phone
+        self.name=name
+        
 
     def emit(self, record):
         log_message = self.format(record)
-        if "connection closed" in log_message.lower():  # Case-insensitive check
-            self.callback(self.transcript)
+        if "connection closed" in log_message.lower():
+            self.callback(self.transcript,self.phone,self.name)
 
-# Function to execute when "connection closed" is detected
-def execute_on_connection_closed(transcript):
-    print("Connection closed detected! Executing custom logic...")
+
+def execute_on_connection_closed(transcript,phone):
+    print("Connection closed detected! Executing Closing Code...")
+    print(phone)
+    #process_lead_conversation
+    print(transcript)
+    print("Summary being generated...")
+    summary = generate_summary(transcript)
+    print(summary)
+
+
+def execute_on_connection_closed_lead(transcript,phone,name):
+    print("Connection closed detected! Executing Closing Code...")
+    process_lead_conversation(transcript,phone,name)
+
+
+def execute_on_connection_closed_support(transcript,phone):
+    print("Connection closed detected! Executing Closing Code...")
+    print(phone)
+    #process_lead_conversation
     print(transcript)
     print("Summary being generated...")
     summary = generate_summary(transcript)
